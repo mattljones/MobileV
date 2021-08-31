e03843ee9f6b8c5d4c460a945d8e456366cc63b6
Matt <joneslewismatthew@gmail.com>
2021-08-31 18:54:46 +0100

    Remove persistent login

--- MobileApp/mobilev/lib/main.dart
+++ MobileApp/mobilev/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 // Module imports
 import 'package:mobilev/services/database_service.dart';
 import 'package:mobilev/services/notification_service.dart';
-import 'package:mobilev/services/network_service.dart';
 import 'package:mobilev/config/routes.dart';
 import 'package:mobilev/config/theme.dart';
 
@@ -19,23 +18,17 @@ void main() async {
   seedDatabase
       ? await databaseService.initSeed()
       : await databaseService.init();
-  // Do not require sign in if user closed the app without signing out
-  bool hasToken = await NetworkService.checkHasToken();
-  String initialRoute = hasToken ? '/my-home' : '/login';
-  runApp(MobileVApp(initialRoute));
+  runApp(MobileVApp());
 }
 
 class MobileVApp extends StatelessWidget {
-  final String initialRoute;
-  MobileVApp(this.initialRoute);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MobileV',
       theme: theme,
-      initialRoute: initialRoute,
+      initialRoute: '/login',
       routes: routes,
     );
   }
--- MobileApp/mobilev/lib/services/network_service.dart
+++ MobileApp/mobilev/lib/services/network_service.dart
@@ -37,12 +37,6 @@ class NetworkService {
     return refreshToken!;
   }
 
-  // Checks if the user was logged in when last closed the app
-  static Future<bool> checkHasToken() async {
-    String? accessToken = await storage.read(key: 'accessToken');
-    return accessToken != null ? true : false;
-  }
-
   // Delete access and refresh tokens
   static Future<void> deleteTokens() async {
     await storage.deleteAll();
